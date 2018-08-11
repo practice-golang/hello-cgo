5ac114101e7e92c851e11e300f06e13b4f356841
Practice Golang <practice.golang@gmail.com>
2018-08-12 01:51:39 +0900

    init

---
+++ src/hello/hello.go
@@ -0,0 +1,17 @@
+package main
+
+// #include <stdio.h>
+// #include <stdlib.h>
+import "C"
+import "unsafe"
+
+// MyCprint - 이럴거면 뭐하러 go를 쓰냐 C/C++을 쓰지
+func MyCprint(s string) {
+	cs := C.CString(s)
+	C.fputs(cs, (*C.FILE)(C.stdout))
+	C.free(unsafe.Pointer(cs))
+}
+
+func main() {
+	MyCprint("This is Print from Cgo ^-^")
+}
