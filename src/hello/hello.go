bd0987e453dfb66c4ed34ca716a50e768d997427
Gopher Kid <42208823+practice-golang@users.noreply.github.com>
2018-08-18 01:20:05 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -5,7 +5,7 @@ package main
 import "C"
 import "unsafe"
 
-// MyCprint - 이럴거면 뭐하러 go를 쓰냐 C/C++을 쓰지
+// MyCprint - 이럴거면 뭐하러 go를 쓰냐 C/C++을 쓰지. 뭐 필요한 곳이 있겠지...
 func MyCprint(s string) {
 	cs := C.CString(s)
 	C.fputs(cs, (*C.FILE)(C.stdout))
