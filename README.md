51c68de3f30ec01374c4b889c606cf45f8b95b53
Practice Golang <practice.golang@gmail.com>
2023-06-02 13:48:14 +0900

    Update README.md

--- README.md
+++ README.md
@@ -8,28 +8,14 @@
 ## Folders
 * `hello` - C codes are embeded
 * `hello-again` - C codes are separated
-* `calc` - Use MSVC
+* `calc` - MSVC usage
+* `structs` - C++ vector - Not working, why???
 
 ## Build
-* hello - Just run `go build` or `go install`
-* hello-again
-    * MinGW - Use make. See `hello-again/Makefile`
+* MSVC
 ```powershell
-cd hello-again
+build.cmd
+```
+```powershell
 mingw32-make.exe
-```
-* calc
-    * MSVC - See [calc/build.cmd](calc/build.cmd)
-```dos
-./msvc_env_2022.ps1
-cd calc
-./build.cmd
-```
-* structs
-    * MSVC - See [structs/build.cmd](structs/build.cmd)
-    * Go is weak very much for c++ struct/class/type
-```powershell
-./msvc_env_2022.ps1
-cd structs
-./build.cmd
 ```
--- structs/company/company.cpp
+++ structs/company/company.cpp
@@ -12,6 +12,38 @@ void* init_company() {
     return c;
 }
 
+void generate_employee_list_vector(void* cmp, int count) {
+    company* c = (company*)cmp;
+
+    std::vector<employee_data> employees = std::vector<employee_data>();
+    employees.reserve(count);
+
+    for (int i = 0; i < count; i++) {
+        employee_data emp;
+
+        emp.id = i;
+
+        int name_length = 5 + (rand() % 5);
+        emp.name = new char[name_length + 1];
+        for (int j = 0; j < name_length; j++) {
+            emp.name[j] = 'a' + (rand() % 26);
+        }
+
+        emp.salary = 5000.0f + (rand() % 5000);
+        employees.emplace_back(emp);
+
+        printf("  %d: %s, $%.2f\n", emp.id, emp.name, emp.salary);
+    }
+
+    c->emp_list = new employee_list{employees.data(), employees.size(), false};
+}
+
+void free_company_vector(void* cmp) {
+    company* c = (company*)cmp;
+    delete (employee_list*)c->emp_list;
+    delete c;
+}
+
 void generate_employee_list_cmalloc(void* cmp, int count) {
     company* c = (company*)cmp;
 
@@ -40,32 +72,6 @@ void generate_employee_list_cmalloc(void* cmp, int count) {
     c->emp_list = emp_list;
 }
 
-void generate_employee_list_vector(void* cmp, int count) {
-    company* c = (company*)cmp;
-
-    std::vector<employee_data> employees = std::vector<employee_data>();
-    employees.reserve(count);
-
-    for (int i = 0; i < count; i++) {
-        employee_data emp;
-
-        emp.id = i;
-
-        int name_length = 5 + (rand() % 5);
-        emp.name = new char[name_length + 1];
-        for (int j = 0; j < name_length; j++) {
-            emp.name[j] = 'a' + (rand() % 26);
-        }
-
-        emp.salary = 5000.0f + (rand() % 5000);
-        employees.emplace_back(emp);
-
-        printf("  %d: %s, $%.2f\n", emp.id, emp.name, emp.salary);
-    }
-
-    c->emp_list = new employee_list{employees.data(), employees.size(), false};
-}
-
 void free_company_cmalloc(void* cmp) {
     company* c = (company*)cmp;
     employee_list* emp = (employee_list*)c->emp_list;
@@ -78,12 +84,6 @@ void free_company_cmalloc(void* cmp) {
     free(emp->data);
     free(emp);
     free(c);
-}
-
-void free_company_vector(void* cmp) {
-    company* c = (company*)cmp;
-    delete (employee_list*)c->emp_list;
-    delete c;
 }
 
 void generate_employee_list(void* cmp, int count) {
